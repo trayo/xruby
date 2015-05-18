5598c7719c386d921782d221050f16894fc89454
Travis Yoder <trayoda@gmail.com>
2015-05-17 18:28:12 -0600

    gives the hello-world tests more descriptive names

--- hello-world/hello_world_test.rb
+++ hello-world/hello_world_test.rb
@@ -8,21 +8,21 @@ rescue LoadError => e
 end
 
 class HelloWorldTest < Minitest::Test
-  def test_no_name
+  def test_say_hello_world
     assert_equal 'Hello, world!', HelloWorld.hello, 'When giving no name, we should greet the world!'
   end
 
-  def test_sample_name
+  def test_say_hello_to_alice
     skip
     assert_equal 'Hello, Alice!', HelloWorld.hello('Alice'), 'When giving "Alice" we should greet Alice!'
   end
 
-  def test_other_sample_name
+  def test_say_hello_to_bob
     skip
     assert_equal 'Hello, Bob!', HelloWorld.hello('Bob'), 'When giving "Bob" we should greet Bob!'
   end
 
-  def test_no_strange_name
+  def test_say_hello_to_empty_string
     skip
     assert_equal 'Hello, !', HelloWorld.hello(''), 'When giving an empty string, it is strange, but should have a space and punctuation'
   end
